ee0437ff4da866f24d1f7508fbf0a9e8d33e2314
RustyBalboadev <38148834+RustyBalboadev@users.noreply.github.com>
2020-02-24 21:26:53 -0600

    Update main.py

--- main.py
+++ main.py
@@ -19,17 +19,24 @@ auth = tweepy.OAuthHandler(API_KEY, API_Secret)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 while True:
-    timelineList = api.user_timeline(id='YOUR TWITTER ID', count=1)
+    timelineList = api.user_timeline(id='YOURTWITTERID', count=1)
     timeline = timelineList[0]
     json_str = json.dumps(timeline._json)
     json_str = json.loads(json_str)
     like_amount = str(json_str['favorite_count'])
     retweet_amount = str(json_str['retweet_count'])
-    title = "@YOURTWITTER - ❤️{}|↪️{}".format(like_amount, retweet_amount)
+    title = "@YOUR@HERE - ❤️{}|↪️{}".format(like_amount, retweet_amount)
     content = str(json_str['text'])
+    final_content = """"""
+    i = 0
+    for each in content:
+        final_content += each
+        if i == 55:
+            final_content += '\n'
+        i += 1
     last_title = str(gist.files).split("'")[1]
     gist.edit(
-        files={last_title:github.InputFileContent(content=content, new_name=title)},
+        files={last_title:github.InputFileContent(content=final_content, new_name=title)},
     )
     upload_amount += 1
     os.system('cls')
